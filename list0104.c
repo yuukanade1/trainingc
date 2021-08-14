515439a42966ce0b09eb2db8961b852e5cef3bc8
yuukanade1 <rrr.h.o.f@gmail.com>
2021-08-15 01:35:24 +0900

    add new file

---
+++ list0104.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("こんにちは。私の名前は千々和竜馬です。\n");
+    return 0;
+}
---
+++ list0105.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("こんにちは。\n私の名前は千々和竜馬です。\n");
+    return 0;
+}
