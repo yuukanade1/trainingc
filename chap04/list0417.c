1d4e90a8282b2adbd62c984a617d83281a40b167
yuukanade1 <rrr.h.o.f@gmail.com>
2021-09-08 23:20:59 +0900

    new add file

---
+++ chap04/list0417.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    int height, width;
+
+    puts("長方形を作ります。");
+    printf("高さ:");    scanf("%d", &height);
+    printf("横幅:");    scanf("%d", &width);
+
+    for (i = 1; i <= height; i++) {
+        for (j = 1; j <= width; j++)
+            putchar('*');
+        putchar('\n');
+    }
+    return 0;
+}
