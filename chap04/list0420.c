3c20402236ecd26d1c6a4572871f277cc790a288
yuukanade1 <rrr.h.o.f@gmail.com>
2021-09-12 15:23:44 +0900

    new add file

--- chap04/list0420.c
+++ chap04/list0420.c
@@ -5,7 +5,22 @@ int main(void)
     int retry;
 
     do {
+        int i,no;
 
+        do {
+            printf("正の整数を入力:");
+            scanf("%d", &no);
+            if (no <= 0)
+                puts("\a正の数を入力してください。");
+        } while (no <= 0);
+
+        for (i = 1; i <= no; i++)
+            putchar('*');
+        putchar('\n');
+
+        printf("もう一度？ [Yes...0 / No...9] : ");
+        scanf("%d", &retry);
     } while (retry == 0);
     
+    return 0;
 }
