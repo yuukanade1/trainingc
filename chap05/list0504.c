bd260449271b5a3764204ec389792d35591d4951
yuukanade1 <rrr.h.o.f@gmail.com>
2021-09-16 02:57:21 +0900

    new add file

--- chap05/list0504.c
+++ chap05/list0504.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
+    int i;
+    double x[7];
+
+    for (i = 0; i < 7; i++)
+        x[i] = 0.0;
+
+    for (i = 0; i < 7; i++)
+        printf("x[%d] = %.1f\n", i, x[i]);
     return 0;
 }
