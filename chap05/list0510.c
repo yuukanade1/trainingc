41a5f2816d694f4b47b7d98cd0e3a7de0bb6df14
yuukanade1 <rrr.h.o.f@gmail.com>
2021-09-21 03:06:33 +0900

    new add file

--- chap05/list0510.c
+++ chap05/list0510.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
+#define NUMBER 5
+
 int main(void)
 {
     int i ;
-    int tensu[5];
+    int tensu[NUMBER];
     int sum = 0;
 
-    printf("5人の点数を入力。\n");
-    for (i = 0; i < 5; i++) {
+    printf("%d人の点数を入力。\n", NUMBER);
+    for (i = 0; i < NUMBER; i++) {
         printf("%2d番 : ", i + 1);
         scanf("%d", &tensu[i]);
         sum += tensu[i];
     }
 
     printf("合計点 : %5d\n", sum);
-    printf("平均点 : %5.1f\n", (double)sum / 5);
+    printf("平均点 : %5.1f\n", (double)sum / NUMBER);
     
     return 0;
 }
