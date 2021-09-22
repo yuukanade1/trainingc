e7150c76bf7029355bd9bbe4c721e10f5181bcef
yuukanade1 <rrr.h.o.f@gmail.com>
2021-09-23 02:48:50 +0900

    new add file

--- chap05/list0511.c
+++ chap05/list0511.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+#define NUMBER 5
+
 int main(void)
 {
+    int i;
+    int tensu[NUMBER];
+    int max, min;
+
+    printf("%d人の点数を入力してください。\n", NUMBER);
+    for (i = 0; i < NUMBER; i++) {
+        printf("%2d : ", i + 1);
+        scanf("%d", &tensu[i]);
+    }
+
+    min = max = tensu[0];
+
+    for (i = 0; i < NUMBER; i++) {
+        if (tensu[i] > max) max = tensu[i];
+        if (tensu[i] < min) min = tensu[i];
+    }
+
+    printf("最高点 : %d\n", max);
+    printf("最低点 : %d\n", min);
+    
     return 0;
 }
