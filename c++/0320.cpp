b2d21b671e7c75e224291cb44057b4ddaa3fb5eb
Ryoma Chidiwa <rrr.h.o.f@gmail.com>
2023-03-08 01:31:10 +0900

    add file.

---
+++ c++/0320.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "文字列リテラルと文字リテラルについて。\n";
+    cout << "二十引用符";
+    cout << '"';
+    cout << "で囲まれた\"ABC\"は文字列リテラルです。\n";
+    cout << "単一引用符";
+    cout << '\'';
+    cout << "で囲まれた'A'は文字リテラルです。\n";
+}
